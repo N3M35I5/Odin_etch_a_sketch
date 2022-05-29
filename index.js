bfa9432f308ad5090c6328ed23eecd2be16e892a
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-29 22:12:47 +0530

    Variable grid size added.

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-let size = 16;
-const container=document.querySelector('#container');
-
-for(let i =0; i<size;i++)
+function sketch(gridSize=16)
 {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let j =0; j<size ; j++)
+    for(let i =0; i<size;i++)
     {
-        const col = document.createElement('div');
-        col.classList.add('column');
-        row.appendChild(col);
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(let j =0; j<size ; j++)
+        {
+            const col = document.createElement('div');
+            col.classList.add('column');
+            row.appendChild(col);
+        }
+        if(i==0)
+        {
+            container.replaceChildren(row);        
+        }
+        else
+            container.appendChild(row);
     }
-    container.appendChild(row)
+    const cols = document.querySelectorAll('.column');
+    cols.forEach((col)=>
+    {
+        col.addEventListener('mouseover',()=>
+        {
+            //console.log(col);
+            col.classList.add('bclr');
+        });
+    });
 }
 
-const cols = document.querySelectorAll('.column');
-cols.forEach((col)=>
+let size = 16;
+
+const container=document.querySelector('#container');
+
+const gridBtn = document.querySelector('#gridSize');
+gridBtn.addEventListener('click',()=>
 {
-    col.addEventListener('mouseover',()=>
-    {
-        //console.log(col);
-        col.classList.add('bclr');
-    });
+    size = prompt('Enter the number of grids');
+    sketch(size);
 });
+
+sketch();
